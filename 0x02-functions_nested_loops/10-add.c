c7479437ba765e9819e3627eb35f01c15a6c5e6f
menaujiri <ujirimena@gmail.com>
2023-10-11 16:46:29 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * add - Add two integers and returns the result.
+ *
+ *Description: This function that adds two integers and
+ * 'n1' and 'n2, returns the result.
+ *
+ * @n1: first integer to be added
+ * @n2: Second integer to be added
+ *
+ * Return:sum.
+ *
+ */
+
+
+int add(int n1, int n2)
+{
+	int sum;
+
+
+	sum = n1 + n2;
+	return (sum);
+}
