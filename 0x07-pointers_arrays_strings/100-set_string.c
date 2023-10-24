5e95b914bcbb2e264e655c4c164d48bfa2471650
menaujiri <ujirimena@gmail.com>
2023-10-23 18:41:40 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - the main function
+  *
+  * @s: the function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
