ee654fd4673242bae23e832dd75d8426ac7efce8
menaujiri <ujirimena>
2024-01-15 20:34:16 -0800

    create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
-gcc -shared -o liball.so *.0
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
